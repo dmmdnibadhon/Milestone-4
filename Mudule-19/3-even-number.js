4051d6b9a06f75b9b75983fc3664d3e7d10d43f2
dmmdnibadhon <mdnazmulislambadhon@gmail.com>
2023-05-15 10:47:39 -0700

    15-05-2023

---
+++ Mudule-19/3-even-number.js
@@ -0,0 +1,5 @@
+var number = 2;
+while(number <= 20){
+    console.log(number);
+    number = number + 2;
+}
---
+++ Mudule-19/4-for-loop.js
@@ -0,0 +1,14 @@
+// var number = 0;
+// while(number <= 10){
+//     console.log("While loop");
+//     number++;
+// }
+
+// for( var number = 0; number <= 7; number++){
+//     console.log("For loop");
+// }
+
+
+for(var i = 2; i <= 20; i+=2){
+    console.log("i");
+}
