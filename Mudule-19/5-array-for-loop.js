6de5e521aee5ab4ea0755883263931e7cab265d6
dmmdnibadhon <mdnazmulislambadhon@gmail.com>
2023-05-17 11:50:01 -0700

    16-05-2023

--- Mudule-19/5-array-for-loop.js
+++ Mudule-19/5-array-for-loop.js
@@ -1,1 +1,9 @@
-for(var i = 0; i <= )
+// for(var i = 0; i <= 7; i++){
+//     console.log(i);
+// }
+
+var numbers = [12, 20, 30, 40, 50, 60];
+for(var i = 0; i <= 5; i++){
+    var number = numbers[i];
+    console.log(number);
+}
---
+++ Mudule-19/8-practice.js
@@ -0,0 +1,32 @@
+var momGiven = 1000;
+var buyApples = 400;
+var buyOranges = 300;
+
+var totalSpent = buyApples + buyOranges;
+console.log("Total spent = " + totalSpent);
+
+var remaing = momGiven - totalSpent;
+console.log("Return = " + remaing);
+
+
+var mathsMarks = 75.25;
+var biologyMarks = 65;
+var chemistry = 80;
+var phisicsMarks = 35;
+var banglaMarks = 99.50;
+
+var totalMarks = mathsMarks + biologyMarks + chemistry + phisicsMarks + banglaMarks;
+console.log("Total marks = " + totalMarks);
+
+var averageMarks = totalMarks / 5;
+var averageTow = averageMarks.toFixed(2);
+console.log("Average marks = " + averageTow);
+
+var fpart = "I am going to be";
+var lpart = " an awesome web developer";
+var spart = fpart + lpart;
+console.log("Full part = " + spart);
+
+var smomGiver = 119;
+var reminder = smomGiver % 4;
+console.log("Reminder = " + reminder);
